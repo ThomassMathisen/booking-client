918afd228501e97d0eefcb136bcc3da46c07b849
ThomassMathisen <thomasmathisen@hotmail.com>
2023-01-02 15:27:41 +0100

    Create header component

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,3 +1,4 @@
+import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./list.css";
 
@@ -5,6 +6,7 @@ const List = () => {
   return (
     <div>
       <Navbar />
+      <Header type="list" />
     </div>
   );
 };
